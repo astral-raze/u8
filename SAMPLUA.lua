34be565d8c87e1b13807eca3ad249abbc8535121
astral-raze <144711184+astral-raze@users.noreply.github.com>
2023-09-12 13:49:44 +0500

    Update SAMPLUA.lua

--- SAMPLUA.lua
+++ SAMPLUA.lua
@@ -90,8 +90,7 @@ function imgui.OnDrawFrame()
       imgui.SetNextWindowPos(imgui.ImVec2(sizeX / 2, sizeY / 2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
       imgui.Begin('Information for bot', main_window_state, imgui.WindowFlags.NoResize + imgui.WindowFlags.NoCollapse + imgui.WindowFlags.NoScrollbar)
       imgui.Text(u8'ПРОВЕРКА АВТО ОБНОВЛЕНИЯ НУ ПИЗДЕЦ КТО ЭТОТ LUA ПРИДУМАЛ')
-      imgui.Text(u8'ПРОВЕРКА АВТО ОБНОВЛЕНИЯ НУ ПИЗДЕЦ КТО ЭТОТ LUA ПРИДУМАЛ ВАХУИ')
-          imgui.Text(u8'AUTOUPDATE')
+      imgui.Text(u8'ПРОВЕРКА АВТО ОБНОВЛЕНИЯ НУ ПИЗДЕЦ КТО ЭТОТ LUA ПРИДУМАЛ UPLOAD')
       imgui.End()
     end
   end
